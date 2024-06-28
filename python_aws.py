3f49001e76d3e2c8108baacab1c8c17e37d46b58
Rodrigo Tomio Kumabe <rodrigo.kumabe@gmail.com>
2024-06-28 09:54:53 -0300

    feat: leaks

--- python_aws.py
+++ python_aws.py
@@ -2,8 +2,8 @@ import boto3
 # configure your ssm client here, such as AWS key or region
 ssm_client = boto3.client('ssm',
     region_name="us-east-1",
-    aws_access_key_id="AKIAIOSFODNN7EXAMPLE",
-    aws_secret_access_key="wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    aws_access_key_id="AKIEXAMPLEEQPQR12345",
+    aws_secret_access_key="EXAMPLEFgSm0xPRIvaTeKB8Nt5LX97EAnEKa12345"
 )
 # then pass to the store
 store = SSMParameterStore(ssm_client=ssm_client)
