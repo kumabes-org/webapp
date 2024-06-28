598a381d51c1becfe978570c66945422a8262ef0
Rodrigo Tomio Kumabe <rodrigo.kumabe@gmail.com>
2024-06-28 16:30:47 -0300

    feat: leaks

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 const port = 3000;
+const BEARER_TOKEN = "ghp_SEcreTS3gOh2VfulanoyDtjmEITAT012345";
 
 app.get("/", (req, res) => {
   res.render("index", {
